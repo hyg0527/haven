996016280fcd0b259fcb7e57923a7c49c53ffe89
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-08-06 15:19:03 +0900

    widget_test.dart 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:haven/main.dart';
+import 'package:haven/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
