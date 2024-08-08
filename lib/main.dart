4825e2ca038e5ee2e18602f7ba0404f7880775c9
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-08-08 11:30:14 +0900

    Home 화면 배치 추가

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Positioned(
               left: 20,
-              top: 189,
+              top: 139,
               child: Container(
                 width: 350,
-                height: 260,
+                height: 229,
                 padding: const EdgeInsets.all(10),
                 decoration: ShapeDecoration(
                   color: Colors.white,
@@ -52,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   shadows: const [
                     BoxShadow(
                       color: Color(0x19000000),
-                      blurRadius: 20,
+                      blurRadius: 10,
                       offset: Offset(0, 0),
                       spreadRadius: 0,
                     )
@@ -67,7 +67,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 100,
                       height: 100,
                       clipBehavior: Clip.antiAlias,
-                      decoration: const BoxDecoration(color: Colors.white),
+                      decoration: const BoxDecoration(),
+                      child: const FlutterLogo(),
                     ),
                     const SizedBox(height: 10),
                     const Text(
@@ -82,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     const SizedBox(height: 10),
                     const Text(
-                      '카메라를 실행시켜 얼굴을 인식해주세요.',
+                      '카메라를 실행시켜 얼굴을 인식해주세요',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         color: Color(0xFF8E8E93),
@@ -98,10 +99,10 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Positioned(
               left: 20,
-              top: 483,
+              top: 388,
               child: Container(
                 width: 350,
-                height: 260,
+                height: 229,
                 padding: const EdgeInsets.all(10),
                 decoration: ShapeDecoration(
                   color: Colors.white,
@@ -111,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   shadows: const [
                     BoxShadow(
                       color: Color(0x19000000),
-                      blurRadius: 20,
+                      blurRadius: 10,
                       offset: Offset(0, 0),
                       spreadRadius: 0,
                     )
@@ -126,11 +127,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 100,
                       height: 100,
                       clipBehavior: Clip.antiAlias,
-                      decoration: const BoxDecoration(color: Colors.white),
+                      decoration: const BoxDecoration(),
+                      child: const FlutterLogo(),
                     ),
                     const SizedBox(height: 10),
                     const Text(
-                      '결과 기록보기',
+                      '입퇴실 기록보기',
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 20,
@@ -140,18 +142,118 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                     const SizedBox(height: 10),
-                    const SizedBox(
-                      width: 179,
-                      child: Text(
-                        '사진을 분석한 결과를\n한번에 볼 수 있어요!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Color(0xFF8E8E93),
-                          fontSize: 16,
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w400,
-                          height: 0,
-                        ),
+                    const Text(
+                      '입퇴실의 기록 결과를 한번에 볼 수 있어요',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF8E8E93),
+                        fontSize: 16,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 680,
+              child: Container(
+                width: 164,
+                height: 110,
+                padding: const EdgeInsets.all(10),
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  shadows: const [
+                    BoxShadow(
+                      color: Color(0x19000000),
+                      blurRadius: 10,
+                      offset: Offset(0, 0),
+                      spreadRadius: 0,
+                    )
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 80,
+                      height: 40,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: const BoxDecoration(),
+                      child: const FlutterLogo(),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'AI 추천 자리',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w600,
+                        height: 0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 206,
+              top: 680,
+              child: Container(
+                width: 164,
+                height: 110,
+                padding: const EdgeInsets.all(10),
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  shadows: const [
+                    BoxShadow(
+                      color: Color(0x19000000),
+                      blurRadius: 10,
+                      offset: Offset(0, 0),
+                      spreadRadius: 0,
+                    )
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 80,
+                      height: 40,
+                      padding: const EdgeInsets.only(
+                          top: 4, left: 25, right: 25, bottom: 3),
+                      clipBehavior: Clip.antiAlias,
+                      decoration: const BoxDecoration(),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      '일반 자리 선택',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w600,
+                        height: 0,
                       ),
                     ),
                   ],
@@ -226,12 +328,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             const Positioned(
               left: 78,
-              top: 80,
+              top: 75,
               child: Text(
                 'Haven',
                 style: TextStyle(
                   color: Color(0xFF33C284),
-                  fontSize: 24,
+                  fontSize: 28,
                   fontFamily: 'Aclonica',
                   fontWeight: FontWeight.w400,
                   height: 0,
@@ -240,13 +342,27 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Positioned(
               left: 20,
-              top: 67,
+              top: 63,
               child: Container(
                 width: 50,
                 height: 50,
                 clipBehavior: Clip.antiAlias,
                 decoration: const BoxDecoration(),
                 child: const FlutterLogo(),
+              ),
+            ),
+            const Positioned(
+              left: 24,
+              top: 645,
+              child: Text(
+                '자리 선택',
+                style: TextStyle(
+                  color: Color(0xFF8E8E93),
+                  fontSize: 16,
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w600,
+                  height: 0,
+                ),
               ),
             ),
           ],
