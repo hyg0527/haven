8dd6aa7e183a3277cceff1d2956d6e0d7abe1c18
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2024-08-17 17:35:37 +0900

    얼굴 캡처기능 구현

--- lib/camera.dart
+++ lib/camera.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'dart:io';
+import 'dart:math';
 import 'package:haven/clickbutton.dart';
 
 class CameraScreen extends StatefulWidget {
@@ -15,6 +17,7 @@ class _CameraScreenState extends State<CameraScreen> {
   CameraController? _controller;
   late Future<void> _initializeController;
   CameraDescription? frontCamera;
+  XFile? _capturedImage;
 
   @override
   void initState() {
@@ -53,6 +56,20 @@ class _CameraScreenState extends State<CameraScreen> {
     }
   }
 
+  // 사진 찍기
+  Future<void> _takePicture() async {
+    try {
+      await _initializeController;
+      final image = await _controller!.takePicture();
+
+      setState(() {
+        _capturedImage = image;
+      });
+    } catch (e) {
+      debugPrint('Cannot take picture: $e');
+    }
+  }
+
   @override
   void dispose() {
     _controller?.dispose();
@@ -86,8 +103,27 @@ class _CameraScreenState extends State<CameraScreen> {
                   if (snapshot.connectionState == ConnectionState.done &&
                       _controller != null) {
                     return AspectRatio(
-                        aspectRatio: _controller!.value.aspectRatio,
-                        child: CameraPreview(_controller!));
+                      aspectRatio: _controller!.value.aspectRatio,
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            CameraPreview(_controller!), // 카메라 미리보기
+                            if (_capturedImage != null)
+                              Transform(
+                                alignment: Alignment.center,
+                                transform: Matrix4.rotationY(pi),
+                                child: Image.file(
+                                  File(_capturedImage!.path),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    );
                   } else if (snapshot.hasError) {
                     return Center(child: Text('Error: ${snapshot.error}'));
                   } else {
@@ -105,34 +141,37 @@ class _CameraScreenState extends State<CameraScreen> {
               child: Center(
                 child: ClickButton(
                   isCircle: true,
-                  child: ClipOval(
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      padding: const EdgeInsets.all(10),
-                      decoration: ShapeDecoration(
-                        color: const Color(0xFF33C284),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
+                  child: GestureDetector(
+                    onTap: _takePicture,
+                    child: ClipOval(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        padding: const EdgeInsets.all(10),
+                        decoration: ShapeDecoration(
+                          color: const Color(0xFF33C284),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          shadows: const [
+                            BoxShadow(
+                              color: Color(0x3F000000),
+                              blurRadius: 10,
+                              offset: Offset(0, 0),
+                              spreadRadius: 0,
+                            )
+                          ],
                         ),
-                        shadows: const [
-                          BoxShadow(
-                            color: Color(0x3F000000),
-                            blurRadius: 10,
-                            offset: Offset(0, 0),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: const Center(
-                        child: Text(
-                          '인식하기',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w600,
-                            height: 1,
+                        child: const Center(
+                          child: Text(
+                            '인식하기',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w600,
+                              height: 1,
+                            ),
                           ),
                         ),
                       ),
